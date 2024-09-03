24587fca5145c89b73d9b1c362cdefbd2d31924e
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-09-03 01:24:26 -0400

    Add annualized version

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,6 +1,7 @@
 from datetime import date
 from dataclasses import dataclass
 from collections import defaultdict
+from typing import TypedDict
 
 from src.stock import Stock
 
@@ -11,17 +12,32 @@ class Portfolio:
     Portfolio representation
     """
 
+    class PortfolioDetails(TypedDict):
+        annualized_return_percentage: float
+        percent_return: float
+
     stocks: dict[Stock, int]  # stock, quantity
 
-    def profit(self, start_date: date, end_date: date) -> float:
+    def profit(self, start_date: date, end_date: date) -> tuple[float, PortfolioDetails]:
         """
         Returns the profit made between the period of time within `start_date` and `end_date`
         """
         stock_profits = [
             (stock.price(end_date) - stock.price(start_date)) * quantity for (stock, quantity) in self.stocks.items()
         ]
+        final_profit = sum(stock_profits)
 
-        return sum(stock_profits)
+        final_outcome = sum([stock.price(end_date) * quantity for (stock, quantity) in self.stocks.items()])
+        initial_investment = sum([stock.price(start_date) * quantity for (stock, quantity) in self.stocks.items()])
+
+        time_period = (end_date - start_date).days / 365
+
+        return final_profit, {
+            "annualized_return_percentage": ((final_outcome / initial_investment) ** (1 / time_period)) - 1
+            if time_period > 0
+            else 1.0,
+            "percent_return": final_outcome / initial_investment,
+        }
 
     @classmethod
     def from_csv(cls, lines: list[str], quantities: dict[str, int]):
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -19,16 +19,19 @@ def portfolio_fixture(stock_file_lines):
 
 
 @pytest.mark.parametrize(
-    "start_date, end_date, expected_profit",
+    "start_date, end_date, expected_profit, expected_annualized_return_percentage",
     [
-        (date(2024, 9, 1), date(2024, 9, 1), 0.0),
-        (date(2024, 9, 1), date(2024, 9, 2), 19.25),
-        (date(2024, 9, 1), date(2024, 9, 3), 13.55),
-        (date(2024, 9, 3), date(2024, 9, 9), 111.4),
-        (date(2024, 9, 1), date(2024, 9, 10), 143.25),
+        (date(2024, 9, 1), date(2024, 9, 1), 0.0, 1.0),
+        (date(2024, 9, 1), date(2024, 9, 2), 19.25, 1.3),
+        (date(2024, 9, 1), date(2024, 9, 3), 13.55, 0.34),
+        (date(2024, 9, 3), date(2024, 9, 9), 111.4, 1.23),
+        (date(2024, 9, 1), date(2024, 9, 10), 143.25, 0.98),
     ],
 )
-def test_portfolio(portfolio_fixture, start_date, end_date, expected_profit):
-    result = portfolio_fixture.profit(start_date, end_date)
+def test_portfolio_profit(
+    portfolio_fixture, start_date, end_date, expected_profit, expected_annualized_return_percentage
+):
+    (result, details) = portfolio_fixture.profit(start_date, end_date)
 
     assert round(result, 2) == expected_profit
+    assert round(details["annualized_return_percentage"], 2) == expected_annualized_return_percentage
